f7da34f75d088059202333fe0424e6372bf73dc6
Rob Wooten <9147757@gmail.com>
2014-10-27 23:09:08 -0500

    starting new controllers

---
+++ api/controllers/MeetingController.js
@@ -0,0 +1,51 @@
+/**
+ * MeetingController
+ *
+ * @description :: Server-side logic for managing meetings
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `MeetingController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `MeetingController.addguest()`
+   */
+  addguest: function (req, res) {
+    return res.json({
+      todo: 'addguest() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `MeetingController.removeguest()`
+   */
+  removeguest: function (req, res) {
+    return res.json({
+      todo: 'removeguest() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `MeetingController.getcenter()`
+   */
+  getcenter: function (req, res) {
+    return res.json({
+      todo: 'getcenter() is not implemented yet!'
+    });
+  }
+};
+
---
+++ api/controllers/VenuesController.js
@@ -0,0 +1,31 @@
+/**
+ * VenuesController
+ *
+ * @description :: Server-side logic for managing venues
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `VenuesController.findvenuesnear()`
+   */
+  findvenuesnear: function (req, res) {
+    return res.json({
+      todo: 'findvenuesnear() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `VenuesController.getvenuedetails()`
+   */
+  getvenuedetails: function (req, res) {
+    return res.json({
+      todo: 'getvenuedetails() is not implemented yet!'
+    });
+  }
+};
+
