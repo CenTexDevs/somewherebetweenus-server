58c9c8a5210a60a3654d213130b651a85fcfaf84
Rob Wooten <9147757@gmail.com>
2014-12-17 16:53:52 -0600

    configuring mongo on production

--- config/connections.js
+++ config/connections.js
@@ -73,7 +73,7 @@ module.exports.connections = {
   productionMongoHqDb: {
     adapter: 'sails-mongo',
     url: process.env.MONGOHQ_URL
-  }
+  },
 
   /***************************************************************************
   *                                                                          *
