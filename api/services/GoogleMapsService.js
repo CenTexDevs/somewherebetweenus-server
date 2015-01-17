03440ce003cc7913b14c8eacd4b1e3933e0b03ca
Rob Wooten <9147757@gmail.com>
2015-01-17 00:04:14 -0600

    error handling]

--- api/services/GoogleMapsService.js
+++ api/services/GoogleMapsService.js
@@ -14,7 +14,9 @@ module.exports = {
         request(_url, function(error, response, body) {
 	    //console.log(body);
             var obj = JSON.parse(body);
-            coordinates = {latitude:(obj.results[0].geometry.location.lat),longitude:(obj.results[0].geometry.location.lng)};
+            if(obj.status == "OK")
+                if(obj.results[0] != null)
+                    coordinates = {latitude:(obj.results[0].geometry.location.lat),longitude:(obj.results[0].geometry.location.lng)};
             
             console.log('in getGeoLocationFromAddress, location is '+ JSON.stringify(coordinates));
 
